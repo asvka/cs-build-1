92b37d2995c31ca67f79d52e4dd902917e46b2e7
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-07-23 19:15:40 -0700

    Removed server, set up react-scripts, created grid/cells

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom'
 import './App.css';
+import Welcome from './components/welcome'
+import Game from './components/game'
+
 import axios from 'axios'
 
-function App() {
+export default function App() {
   return (
+    <main>
     <div className='App'>
-      <h2>I'm test text</h2>
+      <Route exact path='/' component={Game}/>
     </div>
+    </main>
   );
 }
 
-export default App;
---
+++ game-of-life/src/components/welcome.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+export default function Welcome() {
+  return (
+    <div>
+      <h1>WELCOME TO MY TEST</h1>
+    </div>
+  );
+}
